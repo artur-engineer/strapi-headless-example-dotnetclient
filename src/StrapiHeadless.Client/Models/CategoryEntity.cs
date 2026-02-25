1065bb8afb55dd4b777dac75581895e11c5a81f9
aziemianski <artzie92@gmail.com>
2026-02-25 18:25:09 +0100

    Product and category entities

--- src/StrapiHeadless.Client/Models/CategoryEntity.cs
+++ src/StrapiHeadless.Client/Models/CategoryEntity.cs
@@ -1,6 +1,15 @@
-﻿namespace StrapiHeadless.Client.Models;
+﻿using Newtonsoft.Json;
+
+namespace StrapiHeadless.Client.Models;
 
 public class CategoryEntity
 {
+    [JsonProperty("id")]
+    public int Id { get; set; }
     
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    
+    [JsonProperty("documentId")]
+    public string? DocumentId { get; set; }
 }
--- src/StrapiHeadless.Client/Models/ProductEntity.cs
+++ src/StrapiHeadless.Client/Models/ProductEntity.cs
@@ -1,6 +1,36 @@
-﻿namespace StrapiHeadless.Client.Models;
+﻿using Newtonsoft.Json;
+
+namespace StrapiHeadless.Client.Models;
 
 public class ProductEntity
 {
-    
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("documentId")]
+    public string? DocumentId { get; set; }
+
+    [JsonProperty("title")]
+    public required string Title { get; set; }
+
+    [JsonProperty("description")]
+    public required string Description { get; set; }
+
+    [JsonProperty("price")]
+    public decimal Price { get; set; }
+
+    [JsonProperty("createdAt")]
+    public DateTimeOffset CreatedAt { get; set; }
+
+    [JsonProperty("updatedAt")]
+    public DateTimeOffset UpdatedAt { get; set; }
+
+    [JsonProperty("publishedAt")]
+    public DateTimeOffset? PublishedAt { get; set; }
+
+    [JsonProperty("brand")]
+    public string? Brand { get; set; }
+
+    [JsonProperty("category")]
+    public CategoryEntity Category { get; set; }
 }
